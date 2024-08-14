cb8f6250133e9e48d111894555b9cf768e05e751
Nour-Alsakka <nour8alsakka@gmail.com>
2024-08-14 12:12:36 +0300

    start signup users

---
+++ app/Http/Controllers/SiteUsersController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SiteUsers;
+use Illuminate\Http\Request;
+
+class SiteUsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SiteUsers $siteUsers)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SiteUsers $siteUsers)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, SiteUsers $siteUsers)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SiteUsers $siteUsers)
+    {
+        //
+    }
+}
