1040b07f019446f473e45f048114e4e175db9dc0
Nour-Alsakka <nour8alsakka@gmail.com>
2024-08-14 09:51:03 +0300

    first commit with Product table

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\ProductsController;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+// Route::get('login', [AuthController::class, 'login'])->name('login');
+// Route::post('login', [AuthController::class, 'login_check'])->name('login_check');
+// Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::group([
+    'prefix' => '/dashboard',
+    'middleware' => ['IsAdmin'],
+    'as' => 'dashboard.'
+], function () {
+
+    Route::get('/', [DashboardController::class, 'index'])->name('index');
+    // Route::post('/upload', [UploadController::class, 'upload_image'])->name('upload');
+    Route::resource('products', ProductsController::class);
+    // Route::resource('authors', AuthorsController::class);
+    // Route::resource('categories', CategoriesController::class);
+});
